d51cc2973db6ebd98d4a5a08f7ae2d0584199626
yushan <hejiahuan.hjh@alibaba-inc.com>
2023-12-22 18:05:16 +0800

    init

---
+++ src/main/java/com/example/codecovdemo/CodecovDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.codecovdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecovDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodecovDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/codecovdemo/CodecovDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codecovdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodecovDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
