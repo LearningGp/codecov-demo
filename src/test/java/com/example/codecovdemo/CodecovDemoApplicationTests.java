8183cb44b6e2674e1c8e91f7857a69bd9ea8ff26
LearningGp <learninggp4.74@gmail.com>
2023-12-25 11:21:09 +0800

    test (#1)
    * init
    
    * init
    
    * add jacoco
    
    * remove test
    
    * add surefire
    
    * add test
    
    * add .codecov.yml
    
    ---------
    
    Co-authored-by: yushan <hejiahuan.hjh@alibaba-inc.com>

--- src/test/java/com/example/codecovdemo/CodecovDemoApplicationTests.java
+++ src/test/java/com/example/codecovdemo/CodecovDemoApplicationTests.java
@@ -1,10 +1,15 @@
 package com.example.codecovdemo;
 
+import com.example.codecovdemo.demos.web.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class CodecovDemoApplicationTests {
+
+    private final StringUtils stringUtils = new StringUtils();
 
     @Test
     void contextLoads() {
